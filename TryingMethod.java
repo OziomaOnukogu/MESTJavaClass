a593bc64b8115ef2f47b89586b4b9f4e9fb487bf
OziomaOnukogu <116321022+OziomaOnukogu@users.noreply.github.com>
2022-10-21 16:07:57 +0000

    putting file in folder

---
+++ TryingMethod.java
@@ -0,0 +1,52 @@
+public class TryingMethod {
+    public static void main(String[] args) {
+       String day = firstmethod(5);
+
+       System.out.println(day);
+        
+    }
+
+    //First Method
+    public static String firstmethod(int dayofTheWeek) {
+        switch (dayofTheWeek) {
+            case 1:
+                return "monday";
+            case 2:
+                 return "tuesday";
+            case 3: 
+                return "wednesday";
+            case 4:
+                 return "thursday";
+            case 5:
+                 return "friday";
+            case 6:
+                 return "saturday";
+            case 7:
+                 return "sunday";
+            default:
+             return "Give a corect number"; 
+        }
+        
+    }
+    //Third Method
+  public class iteractions {
+     public void thirdmethod(String)[] args {
+          int x;
+
+          for (int x = 1; x < 1000; x++) {
+
+               system.out.println(x);
+
+          }   
+     }
+  }
+
+    //Last Method
+    public static void main (string[]args){
+     int sum=0
+     for(int 1=12; i<= 103; i= 12+i)
+             sum = sum + i;
+     system.out.println("sum of 12 to 103")
+    }
+    
+}
